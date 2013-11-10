7b9ec8d151b82312300e2aa047a8fc900f6922a4
Narod Ramburrun <nsrambur@uwaterloo.ca>
2013-11-10 07:51:07 -0500

    Update TimeSlot.cs

--- TimeSlot.cs
+++ TimeSlot.cs
@@ -1,43 +1,56 @@
-class TimeSlot
-{
-    public TimeSlot()
-	{
-		type = 0;
-		day = 0;
-		instructor = "";
-		startTime = 0;
-		length = 0;
-		location = 0;
-	}
-	
-	
-    enum WeekDay { M, T, W, Th, F, S, SU };
-    int day;
-    int startTime;	//indexed by half hour from 0800 please
-    int length;		//in minutes, please
-    string location;
-    
-    public int Day
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+    class TimeSlot
     {
-        get{ return day; }
-        set{ day = value; }
+        public TimeSlot()
+        {
+            int type = 0;
+            day = 0;
+            instructor = "";
+            startTime = 0;
+            length = 0;
+            location = "";
+        }
+
+
+        enum WeekDay { M, T, W, Th, F, S, SU };
+        int day;
+        int startTime;        //indexed by half hour from 0800 please
+        int length;                //in minutes, please
+        string location;
+        private string instructor;
+
+        public int Day
+        {
+            get { return day; }
+            set { day = value; }
+        }
+
+        public int StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+
+        public int Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        public string Instructor
+            {
+                get { return instructor; }
+                set { instructor = value; }
+            }
     }
-    
-    public int StartTime
-    {
-        get{ return startTime; }
-        set{ startTime = value; }
-    }
-    
-    public int Length
-    {
-        get{ return length; }
-        set{ length = value; }
-    }
-    
-    public int Location
-    {
-        get{ return location; }
-        set{ location = value; }
-    }
-}
+
