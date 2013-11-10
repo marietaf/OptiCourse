5f2d5390f1eb932cf671231f06eeffd43f7cf885
treeBoar <killmenowenator@gmail.com>
2013-11-10 00:40:04 -0500

    split into two classes
    update if needed

--- Section.cs
+++ Section.cs
@@ -1,13 +1,58 @@
 class Section
 {
-    enum Type { LECTURE, TUTORIAL, LAB };
-    enum WeekDay { MON, TUE, WED, THU, FRI, SAT, SUN };
+	enum Type { LECTURE, TUTORIAL, LAB };
+	
+	public Section ()
+	{
+		type = 0;
+		instructor = "";
+	}
+	
+	public TimeSlot[] timeSlot;
+	private int type;
+	private string instructor;
+	
+	
+	public int Type
+    {
+        get{ return sectionType; }
+        set
+        { 
+            if( value.ToLower() == "lec" )
+                type = Type.LECTURE;
+            else if( value.ToLower() == "tut" )
+                type = Type.TUTORIAL;
+            else if( value.ToLower() == "lab" )
+                type = Type.LAB;
+        }
+    }
+    
+    public string Instructor
+    {
+        get{ return instructor; }
+        set{ instructor = value; }
+    }
+}
+
+class TimeSlot
+{
+    public TimeSlot()
+	{
+		num = 0;
+		type = 0;
+		day = 0;
+		instructor = "";
+		startTime = 0;
+		length = 0;
+		location = 0;
+	}
+	
+	
+    enum WeekDay { M, T, W, Th, F, S, SU };
     int num;
-    int type;
-    string instructor;
     int day;
-    // add time
-    int length;
+    int startTime;	//indexed by half hour from 0800 please
+    int length;		//in minutes, please
     string location;
     
     int Number
@@ -16,45 +61,27 @@ class Section
         set{ num = value; }
     }
     
-    int Type
-    {
-        get{ return sectionType; }
-        set
-        { 
-            if( value.ToLower() == "lecture" )
-                type = Type.LECTURE;
-            else if( value.ToLower() == "tutorial" )
-                type = Type.TUTORIAL;
-            else if( value.ToLower() == "lab" )
-                type = Type.LAB;
-        }
-    }
     
-    string Instructor
-    {
-        get{ return instructor; }
-        set{ instructor = value; }
-    }
     
-    int Day
+    public int Day
     {
         get{ return day; }
         set{ day = value; }
     }
     
-    ??? Time
+    public int Time
     {
-        get{ return time; }
-        set{ time = value; }
+        get{ return startTime; }
+        set{ startTime = value; }
     }
     
-    int length
+    public int Length
     {
         get{ return length; }
         set{ length = value; }
     }
     
-    int Location
+    public int Location
     {
         get{ return location; }
         set{ location = value; }
