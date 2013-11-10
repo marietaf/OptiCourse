248651bbb06c8cfc9eb1e329fa34e7b19f39474f
Marieta F <marietafarova@gmail.com>
2013-11-09 19:59:32 -0500

    +main, schedule classes
    Temp main until Nikhil adds the GUI

---
+++ OptiCourse.cs
@@ -0,0 +1,9 @@
+using System;
+
+class OptiCourse
+{
+    public void Main()
+    {
+        
+    }
+}
---
+++ Schedule.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Schedule
+{
+    // Arrays for the schedule
+    //Course timeSlotArray[] = new Course[7*28];
+    bool [,] scheduleAvail;
+    int courseCount;
+    
+    public Schedule()
+    {
+        // Create time slots - 7 days with 28 slots per day
+        scheduleAvail = new bool[7, 28];
+        courseCount = 0;
+        
+        // Assume all time slots are free
+        foreach( bool slot in scheduleAvail )
+        {
+            slot = false;
+        }
+    }
+    
+    public void AddCourse()
+    {
+    
+    }
+    
+    public void RemoveCourse()
+    {
+    
+    }
+}
